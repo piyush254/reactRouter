d04b6e08b0b93fbac2276bcb11445284bb67ebfc
piyush254 <76161817+piyush254@users.noreply.github.com>
2024-03-30 20:54:47 +0530

    This is the project of react router

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Component/Header/Header'
+import Footer from './Component/Footer/Footer'
+import Home from './Component/Home/Home'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
